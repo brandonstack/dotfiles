eb03272f9e2395fcc9d4281c3994bb25142fc5af
brandonstack <lxxdev@gmail.com>
2024-01-24 09:46:21 +0800

    add basic config

---
+++ .emacs.d/init.el
@@ -0,0 +1,62 @@
+;;; appearence
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; disable visual scrollbar
+(tool-bar-mode -1)          ; disable the toolbar
+(tooltip-mode -1)           ; diable tooltips
+(set-fringe-mode 10)        ; got some breathing room
+(menu-bar-mode -1)          ; disable menu bar
+
+;; setup visiable bell
+(setq visible-bell t)
+
+;; set font
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 110)
+
+(load-theme 'wombat)
+
+;;; global config
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;;; packages
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package swiper)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
