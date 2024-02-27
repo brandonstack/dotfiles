e304d55fc4dab4a70208d7fc7398abe2669107b8
brandonstack <lxxdev@gmail.com>
2024-02-27 22:00:32 +0800

    add chatgpt-shell

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,7 +13,7 @@
  ;; If there is more than one, they won't work right.
  '(delete-selection-mode nil)
  '(package-selected-packages
-   '(buffer-move pdf-tools json-mode evil-collection eshell-git-prompt virtualenvwrapper eshell-prompt-extras evil-nerd-commenter company-box lsp-treemacs lsp-ui lsp-mode auto-package-update yasnippet-snippets ein dashboard page-break-lines better-defaults elpy plantuml-mode org-cliplink org-roam-ui general evil projectile magit org-roam visual-fill-column org-bullets helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline nerd-icons counsel swiper)))
+   '(chatgpt-shell buffer-move pdf-tools json-mode evil-collection eshell-git-prompt virtualenvwrapper eshell-prompt-extras evil-nerd-commenter company-box lsp-treemacs lsp-ui lsp-mode auto-package-update yasnippet-snippets ein dashboard page-break-lines better-defaults elpy plantuml-mode org-cliplink org-roam-ui general evil projectile magit org-roam visual-fill-column org-bullets helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline nerd-icons counsel swiper)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
