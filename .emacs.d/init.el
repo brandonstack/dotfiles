4cf888025c2412e07228f4264474ff56a0ffb5ff
brandonstack <lxxdev@gmail.com>
2024-03-04 09:07:20 +0800

    test

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,9 +11,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
  '(delete-selection-mode nil)
  '(package-selected-packages
-   '(chatgpt-shell buffer-move pdf-tools json-mode evil-collection eshell-git-prompt virtualenvwrapper eshell-prompt-extras evil-nerd-commenter company-box lsp-treemacs lsp-ui lsp-mode auto-package-update yasnippet-snippets ein dashboard page-break-lines better-defaults elpy plantuml-mode org-cliplink org-roam-ui general evil projectile magit org-roam visual-fill-column org-bullets helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline nerd-icons counsel swiper)))
+   '(code-cells jupyter chatgpt-shell buffer-move pdf-tools json-mode evil-collection eshell-git-prompt virtualenvwrapper eshell-prompt-extras evil-nerd-commenter company-box lsp-treemacs lsp-ui lsp-mode auto-package-update yasnippet-snippets ein dashboard page-break-lines better-defaults elpy plantuml-mode org-cliplink org-roam-ui general evil projectile magit org-roam visual-fill-column org-bullets helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline nerd-icons counsel swiper)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
